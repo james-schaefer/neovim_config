c23c885f8e789a8af74650d10f82c214b31c46e4
james schaefer <jschaefer@playags.com>
2026-03-17 10:44:31 -0700

    vanilla config

---
+++ init.lua
@@ -0,0 +1,42 @@
+--[[
+  james' neo vim config
+]]
+
+vim.g.mapleader = ' '
+
+local o = vim.opt
+o.number = false
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+o.expandtab = true
+o.signcolumn = "yes"
+o.winborder = "rounded"
+
+-- Custom rulerformat shows special characters status
+vim.opt.rulerformat = '%=%l,%c,%p%% %=%{%&list?"tabs visible":"tabs normal"%}%='
+
+-- Enable list mode to show select whitespace characters
+o.list = true
+o.listchars = {
+  tab = '→\\ ',  -- Shows tabs as '→   ' (double backslash for proper escaping)
+  trail = '⍽',   -- Shows trailing spaces as '⍽'
+  nbsp = '⍽'      -- Shows non-breaking spaces as '⍽'
+}
+
+-- Toggle list (whitespace visibility) with <Leader><Tab>
+vim.keymap.set('n', '<Leader><Tab>', function()
+  o.list = not o.list:get()
+end, { desc = 'Toggle whitespace visibility' })
+
+vim.keymap.set('n', '<Leader>0', ':update<CR> :source<CR>') 
+
+-- Configure COPY and PASTE for the system clipboard
+  vim.opt.clipboard = "unnamedplus"
+  vim.keymap.set({'n', 'v', 'x'}, '<Leader>d', '"+d') -- cut to system clipboard
+  vim.keymap.set({'n', 'v', 'x'}, '<Leader>y', '"+y') -- copy to system clipboard
+  vim.keymap.set({'n', 'v', 'x'}, '<Leader>p', '"+p') -- paste from system clipboard
+  -- :checkhealth provider
+  -- determin your window manager: echo #XDG_SESSION_TYPE
+  -- fix for X11: sudo apt install xclip
+  -- fix for Wayland: sudo apt install wl-clipboard
